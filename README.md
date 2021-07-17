4658e5e460d89464973619d0aa220e6d939aba71
onur <onurpolatplt@gmail.com>
2021-07-17 13:06:40 +0200

    first creation

---
+++ README.md
@@ -0,0 +1,1 @@
+# TripAdviser-Front
---
+++ assets/js/app.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollRight = () => {
+    document.getElementById("crsl").scrollLeft += 854.859;
+  };
+  const scrollLeft = () => {
+    document.getElementById("crsl").scrollLeft -= 854.859;
+  };
+  const buttonRight = document.getElementById("arrow-left");
+  buttonRight.addEventListener("click", scrollLeft);
+
+  const buttonLeft = document.getElementById("arrow-right");
+  buttonLeft.addEventListener("click", scrollRight);
+
+  document.querySelector("#btn-contact").addEventListener("click", () => {
+    document
+      .querySelector(".submit-screen-background")
+      .classList.add("display-on");
+  });
+  document
+    .querySelector("#btn-close-submit-box")
+    .addEventListener("click", () => {
+      document
+        .querySelector(".submit-screen-background")
+        .classList.remove("display-on");
+    });
+
+  document
+    .querySelector("#contact-form")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const data = {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        message: document.querySelector("#user-message").value,
+      };
+      console.log(data);
+      document.querySelector("#response").innerHTML = "Merci";
+      const response = await axios.post("http://localhost:3000/contact", data);
+      console.log(response.data);
+      console.log(response.status);
+
+      const msg = document.querySelector("#response");
+      if (response.status === 200) {
+        msg.innerHTML = "Bienvenu sur TripAdviser";
+      } else {
+        msg.innerHTML = "Oops! Une erreur est survenue. Essayez à nouveau";
+      }
+    });
+});
